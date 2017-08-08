d934e89d6548b095561eebd855d802d47afd4bd6
Adrian Ortiz <adrianortiz@Adrians-MacBook-Pro.local>
2017-08-08 09:37:48 -0700

    added buttons + project.html

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import webapp2
 import urllib2
 import json
+import jinja2
+import os
+import logging
+
+first_string = 'Happy'
+second_string = 'Sad'
+jinja_environment = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template = jinja_environment.get_template('project.html')
+        self.response.write(template.render())
+
 
 class SecondHandler(webapp2.RequestHandler):
     def get(self):
