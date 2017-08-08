1a7039fb7ffc33f8b8958778f352bc998080ebb6
Juhi <juhpark@csumb.edu>
2017-08-08 09:49:41 -0700

    Fixning git pulling and commiting errors

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import webapp2
 import urllib2
+import urllib
 import json
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -8,6 +13,11 @@ class MainHandler(webapp2.RequestHandler):
 
 class SecondHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello this is second page')
+        r_template = jinja_environment.get_template('/static/second.html')
+
+        template_variables = {
+
+        }
+        self.response.write(r_template.render(template_variables))
 
 app = webapp2.WSGIApplication([('/', MainHandler),('/second', SecondHandler)], debug = True)
